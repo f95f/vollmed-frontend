e3884590015af84ceedf2d99e1c2ce4c7cabe3a5
f95f <95felipefernandes@gmail.com>
2024-12-27 18:25:08 -0300

    refactor: realocate the dynamic background code to a separated component.

--- vollmed/src/app/app.component.ts
+++ vollmed/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DynamicBackgroundComponent } from './shared/dynamic-background/dynamic-background.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DynamicBackgroundComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ vollmed/src/app/shared/dynamic-background/dynamic-background.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-background',
+  imports: [],
+  templateUrl: './dynamic-background.component.html',
+  styleUrl: './dynamic-background.component.scss'
+})
+export class DynamicBackgroundComponent {
+
+}
